d259826a89be3c57de213d8c1c42026ddf4db680
rodrigo <rodrigomahamud@gmail.com>
2021-01-25 17:05:52 +0100

    base datos try 4

--- php/conexion.php
+++ php/conexion.php
@@ -3,7 +3,7 @@ $connection = mysqli_connect('localhost:3306', 'qlbmlpye', 'The.LOFU-234!');
 if (!$connection){
     die("Fallo la conexion con la base de datos" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'baul');
+$select_db = mysqli_select_db($connection, 'Formulario');
 if (!$select_db){
     die("Database seleccionada ha fallado" . mysqli_error($connection));
 }
